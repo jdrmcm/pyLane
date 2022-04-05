8983c0092faab59e2d558dfaf869fdd6cb97eee0
Josiah McMillan <josiah.mcmillan2@gmail.com>
2022-04-05 17:30:50 +0200

    updated
    updated the json appending function to properly work
    Gave comments on how you could fix the appends in the future or increase functionality
    one issue of parsing jsons is that you have to know how they look when you're receiving them or you're a bit fucked.
    Please note how I pass the file into the class and then use the class structure to save on passing parameters around. It makes life easier!

--- main.py
+++ main.py
@@ -12,67 +12,47 @@ class Entry:
                 "usernames": self.user,
                 "passwords": self.password
             }
+        self.file = ""
 
-
-    def user_input(self):
+    def user_input(self, file):
+        self.file = file
         tag_entry = input("Input a tag: ")
         username_entry = input("Input a username: ")
         password_entry = input("Input a password: ")
         self.user, self.tag, self.password = username_entry, tag_entry, password_entry
-
-
-        # tags.append(tag_entry)
-        # usernames.append(username_entry)
-        # passwords.append(password_entry)
         self.write_info()
 
 
     def write_info(self):
-        with open(file, "r+") as read_file:
+        with open(self.file, "r+") as read_file:
             data = json.load(read_file)
-            print(data)
-            new_entry = {
-                "tags": self.tag ,
-                "usernames": self.user,
-                "passwords": self.password
-            }
+            # This new_entry should only be used if you have a sorting tag to insert each dict underneath
+            # Such an entry might take the form of name or something so your file format would look like
+            # name:
+            #   tags: tag
+            #   username: username
+            #   password: password
+            # Then repeat...
+            #
+            # new_entry = {
+            #     "tags": self.tag,
+            #     "usernames": self.user,
+            #     "passwords": self.password
+            # }
+            # // The error was happening here because this is appending a dictionary where the "Tags" were
+            # // needs instead to append each of the inputs to proper spot.. so instead of:
+            # data.append(new_entry)
+            #  We should have this:
+            data["tags"].append(self.tag)
+            data["usernames"].append(self.user)
+            data["passwords"].append(self.password)
 
-            data.append(new_entry)
             read_file.seek(0)
             # convert back to json.
             json.dump(data, read_file, indent=0)
             print(data)
 
 
-
-
-file = "data.json"
-tags = []
-usernames = []
-passwords = []
-
-
-
-def read_info():
-    with open(file, "r") as read_file:
-        data = json.load(read_file)
-        print(data)
-
-
-
-
-def debug_info():
-    print(
-        tags,
-        usernames,
-        passwords)
-
-
 if __name__ == "__main__":
+    Entry().user_input("data.json")
 
-    Entry().user_input()
-
-debug_info()
-
-read_info()
-debug_info()
