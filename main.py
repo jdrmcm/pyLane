42f9f48c3cbba04a98db0cf9dadfae2688203314
Jordyn McMillan <jdr.mcm@gmail.com>
2022-04-05 14:06:46 -0700

    added search and remove functions closed #1

--- main.py
+++ main.py
@@ -16,6 +16,37 @@ class Entry:
         self.user, self.tag, self.password = username_entry, tag_entry, password_entry
         self.write_info()
 
+    def user_search(self, file, remove):
+        self.file = file
+        tag_search = input("Input a tag: ")
+        with open(self.file, "r") as read_file:
+            data = json.load(read_file)
+        for entry in range(len(data["tags"])):
+            if data["tags"][entry] == tag_search:
+                print("TAG: " + data["tags"][entry])
+                print("USERNAME: " + data["usernames"][entry])
+                print("PASSWORD: " + data["passwords"][entry])
+                if remove:
+                    self.remove_entry(file, entry)
+
+    def remove_entry(self, file, entry):
+        self.file = file
+        print("Are you sure you want to remove this? Type Y or N:")
+        user_input = input("> ")
+        if user_input.lower() == "y":
+            with open(self.file, "r") as read_file:
+                data = json.load(read_file)
+                del data["tags"][entry]
+                del data["usernames"][entry]
+                del data["passwords"][entry]
+                with open(self.file, "w") as write_file:
+                    write_file.seek(0)
+                    json.dump(data, write_file, indent=0)
+                print("Entry deleted.")
+                menu()
+        else:
+            menu()
+
     def write_info(self):
         with open(self.file, "r+") as read_file:
             data = json.load(read_file)
@@ -26,8 +57,26 @@ class Entry:
             read_file.seek(0)
             # convert back to json.
             json.dump(data, read_file, indent=0)
-            print(data)
+            menu()
+
+
+def menu():
+    print(
+        f"Please select a menu option by its number:"
+        f"\n1) New entry"
+        f"\n2) Search by tag"
+        f"\n3) Remove entry")
+    user_input = input("> ")
+    try:
+        match int(user_input):
+            case 1: Entry().user_input("data.json")
+            case 2: Entry().user_search("data.json", False)
+            case 3: Entry().user_search("data.json", True)
+            case _: print("Invalid input")
+    except Exception as e:
+        print(f"[!] ERROR: {e}")
+        menu()
 
 
 if __name__ == "__main__":
-    Entry().user_input("data.json")
+    menu()
